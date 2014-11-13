426a1be1e537ffd34b4f839026e39df86dd31085
Bryson Hill <bryson.hill@gmail.com>
2014-11-13 14:27:05 -0700

    try again

---
+++ app/public/js/controllers/createControl.js
@@ -0,0 +1,28 @@
+var app = angular.module('groupDropper');
+
+app.controller('createControl', function($rootScope, $scope, productService, $location, userService){
+
+$scope.product = {
+		productTitle: '',
+		startingPrice: '',
+		minimumPrice: '',
+		reductionAmount: '',
+		peopleThreshold: '',
+		productPic: ''
+	};
+
+$scope.createProduct = function() {
+	
+		productService.addProduct($scope.product)
+		$scope.product = {
+		productTitle: '',
+		startingPrice: '',
+		minimumPrice: '',
+		reductionAmount: '',
+		peopleThreshold: '',
+		productPic: ''
+	};
+		$location.path('/products');
+	}
+
+	});
---
+++ app/public/js/services/productService.js
@@ -0,0 +1,41 @@
+var app = angular.module('groupDropper')
+app.service('productService', function($http, $q) {
+	
+	this.getProducts = function(){
+		return $http({
+			method: 'GET',
+			url: '/products',
+		}).then(function(res){
+			console.log(res.data);
+			return res.data;
+		})
+	}
+
+	this.addProduct = function(product) {
+		
+		return $http({
+			method: 'POST',
+			url: '/products',
+			data: product
+		})
+	}
+
+	this.getProduct = function(id) {
+	return $http({
+		method: 'GET',
+		url: '/products/' + id
+	})
+}
+
+	// this.castVote = function(id, option){
+	// 	return $http ({
+	// 		method: 'PUT',
+	// 		url: '/vote/' + id,
+	// 		data: option
+	// 	}).then(function(res) {
+	// 		console.log(res.data)
+	// 		return res.data
+	// 	})
+	// }
+
+});
