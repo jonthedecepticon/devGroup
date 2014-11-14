c2dd30c9977cd4381d5df7aac16b9a44b39c6a76
jonthedecepticon <jlambson@hirevue.com>
2014-11-14 02:26:40 -0700

    FB login working

--- app/public/js/models/User.js
+++ app/public/js/models/User.js
@@ -1,23 +1,3 @@
-//  "use-strict";
-// var mongoose = require('mongoose');
-// var Schema = mongoose.Schema;
-
-
-// var User = new Schema({
-//        userName: {type: String},
-//        facebookId: {type: String},
-//        accountCreated: {type: Date},
-//        email: {type: String},
-//        admin: {type: Boolean, default:false},
-//        votedProducts: [ {type: mongoose.Schema.Types.ObjectId, ref: 'Product'} ],
-//        myProducts: [ {type: mongoose.Schema.Types.ObjectId, ref: 'Product'} ]
-
-       
-// })
-
-// module.exports = mongoose.model("User", User);
-
-//-------------mess with this once authenticated w/ FB and Passport-local-----------------
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
 
@@ -31,7 +11,6 @@ var userSchema = new mongoose.Schema({
   twitter: { type: String, unique: true, sparse: true },
   google: { type: String, unique: true, sparse: true },
   
-
   profile: {
     name: { type: String, default: '' },
     gender: { type: String, default: '' },
