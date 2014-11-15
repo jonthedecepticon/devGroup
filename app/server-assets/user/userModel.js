50e5c52f59fc40b4e27409b58eea34952d547d15
Bryson Hill <bryson.hill@gmail.com>
2014-11-14 20:40:31 -0700

    syntax to all files

--- app/server-assets/user/userModel.js
+++ app/server-assets/user/userModel.js
@@ -1,22 +1,3 @@
-//  "use-strict";
-// var mongoose = require('mongoose');
-// var Schema = mongoose.Schema;
-
-
-// var User = new Schema({
-//        userName: {type: String},
-//        facebookId: {type: String},
-//        accountCreated: {type: Date},
-//        email: {type: String},
-//        admin: {type: Boolean, default:false},
-//        votedProducts: [ {type: mongoose.Schema.Types.ObjectId, ref: 'Product'} ],
-//        myProducts: [ {type: mongoose.Schema.Types.ObjectId, ref: 'Product'} ]
-
-       
-// })
-
-// module.exports = mongoose.model("User", User);
-//------------------------------------------------------
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
 
